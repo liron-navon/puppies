5a61951b747094e02ff432242855bb7c2c30795f
Liron Navon <lironavon42@gmail.com>
2021-09-30 09:44:08 +0200

    added tfjs and a set of tools to detect dog breeds (#13)
    added tfjs and a set of tools to detect dog breeds

---
+++ packages/app/src/dog-breed-detector/detectDogBreed.test.ts
@@ -0,0 +1,72 @@
+import "../mocks/tensorflow.mocks";
+import { normalizeDogBreed } from "./detectDogBreed";
+
+import { classifyImage, classifyDogBreeds } from './detectDogBreed';
+
+describe('detectDogBreed', () => {
+    /**
+     * tfjs might return non existent sub breeds of dogs like Eskimo/Siberian husky, 
+     * while the api we work with will only accept husky, 
+     * or with 'Alaskan malamute' where we only have 'malamute' to work with
+     * but for other dogs it might accept the sub breed as well
+     * 
+     * for that reason we need to normalize the dog breed before we can pass it to the dogs api
+     */
+    describe('normalizeDogBreed', () => {
+        test('can find dog breed from image description', () => {
+            expect(normalizeDogBreed('Eskimo dog, husky')).toBe('husky');
+            expect(normalizeDogBreed('Siberian husky')).toBe('husky');
+            expect(normalizeDogBreed('golden retriever')).toBe('golden retriever');
+            expect(normalizeDogBreed('malamute, malemute, Alaskan malamute')).toBe('malamute');
+        });
+    })
+
+
+    describe('detectDogBreed', () => {
+        /**
+         * The app is only meant for dog breeds, a banana is not a dog, 
+         * therefore we need to filter such non dog images
+         */
+        it('fails to detect a banana as a dog', async () => {
+            const image = document.createElement('img');
+            image.id = 'banana'
+            const breeds = await classifyDogBreeds(image);
+            expect(breeds[0].breed).toBe(null);
+            expect(breeds[1].breed).toBe(null);
+            expect(breeds[2].breed).toBe(null);
+        });
+
+        it('can detect golden retriever breed', async () => {
+            const image = document.createElement('img');
+            image.id = 'golden retriever'
+            const breeds = await classifyDogBreeds(image);
+            expect(breeds[0].breed).toBe('golden retriever');
+            expect(breeds[1].breed).toBe('cocker spaniel');
+            expect(breeds[2].breed).toBe('retriever');
+        });
+
+        it('can detect huskey breed', async () => {
+            const image = document.createElement('img');
+            image.id = 'huskey'
+            const breeds = await classifyDogBreeds(image);
+            expect(breeds[0].breed).toBe('husky');
+            expect(breeds[1].breed).toBe('husky');
+            expect(breeds[2].breed).toBe('malamute');
+        });
+
+        it('can handle errors', () => {
+            const image = document.createElement('img');
+            image.id = 'error'
+            expect(classifyDogBreeds(image)).rejects.toEqual('mocked test error');
+        });
+    })
+
+    it('can provide predictions for a huskey image', async () => {
+        const image = document.createElement('img');
+        image.id = 'huskey'
+        const predictions = await classifyImage(image);
+        expect(predictions[0].className).toBe('Eskimo dog, husky');
+        expect(predictions[1].className).toBe('Siberian husky');
+        expect(predictions[2].className).toBe('malamute, malemute, Alaskan malamute');
+    });
+})
---
+++ packages/app/src/dog-breed-detector/detectDogBreed.ts
@@ -0,0 +1,61 @@
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import '@tensorflow/tfjs';
+import breedsMap from './breedsMap.json';
+
+export interface Prediction {
+    className: string;
+    probability: number;
+}
+
+export interface DogBreedPrediction {
+    breed: string | null;
+    probability: number;
+}
+
+// used to memorize the mobilenet for future use
+let model: mobilenet.MobileNet;
+export const classifyImage = async (img: ImageData | HTMLImageElement) => {
+    if (!model) {
+        model = await mobilenet.load();
+    }
+    return model.classify(img);
+}
+
+// given a classification of an image of a dog, find the exact breed from the dog breeds map
+export const normalizeDogBreed = (breedDescription: string): string | null => {
+    const words = breedDescription.match(/\b(\w+)\b/g);
+    if (!words) {
+        return null;
+    }
+
+    // find the dog breed
+    for (let i = 0; i < words.length; i++) {
+        const breed = words[i] as string;
+        if (breed in breedsMap) {
+            const subBreedsList = (breedsMap as Record<string, string[]>)[breed];
+            if (subBreedsList && subBreedsList.length) {
+                // find the sub breed
+                for (let k = 0; k < words.length; k++) {
+                    const subBreed = words[k];
+                    if (subBreedsList.includes(subBreed)) {
+                        return `${subBreed} ${breed}`
+                    }
+                }
+            }
+            return breed
+        }
+    }
+    return null
+}
+
+// given an image of a dog, detect the most probable dog breed
+export const classifyDogBreeds = async (img: ImageData | HTMLImageElement): Promise<DogBreedPrediction[]> => {
+    const results = await classifyImage(img);
+    return results.map(result => {
+        const breed = normalizeDogBreed(result.className);
+        return {
+            breed,
+            probability: result.probability
+        }
+    })
+}
