a6e764ea6a805608cb861033b3518835e255efc9
liron-navon <liron.navon@rabobank.nl>
2021-10-01 15:06:09 +0200

    Create jest-puppeteer.config.js

---
+++ packages/components/jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    launch: {
+      headless: true,
+      ignoreDefaultArgs: ["--disable-extensions"],
+      args: ["--no-sandbox"],
+    //   executablePath: "chrome.exe"
+    }
+  };
