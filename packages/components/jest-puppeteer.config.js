62fb65561fddc8587e576ed180e0cab479b14714
liron-navon <liron.navon@rabobank.nl>
2021-10-01 23:57:36 +0200

    Update jest-puppeteer.config.js

--- packages/components/jest-puppeteer.config.js
+++ packages/components/jest-puppeteer.config.js
@@ -1,8 +1,17 @@
 const port = process.env.PORT || 6006;
 
 let options = {
-    headless: true,
-    args: ["--no-sandbox"],
+    launch: {
+        headless: true,
+        devtools: true,
+        args: [
+          '--ignore-certificate-errors',
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-accelerated-2d-canvas',
+          '--disable-gpu'
+              ]
+    },
     server: {
         command: `start-storybook -p ${port} --ci --quiet`,
         port: port,
