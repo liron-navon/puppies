e324bd0e0774c63a7f87a390912b99354d8fdf2f
liron-navon <liron.navon@rabobank.nl>
2021-10-01 23:39:13 +0200

    ci

--- packages/components/jest-puppeteer.config.js
+++ packages/components/jest-puppeteer.config.js
@@ -1,17 +1,9 @@
-// module.exports = {
-//     launch: {
-//       headless: true,
-//       ignoreDefaultArgs: ["--disable-extensions"],
-//       args: ["--no-sandbox"],
-//     //   executablePath: "chrome.exe"
-//     }
-//   };
-
 let options = {};
 
 if(process.env.CI === 'true') {
     options = {
-        headless: true
+        headless: true,
+        args: ["--no-sandbox"],
     }
 }
 
