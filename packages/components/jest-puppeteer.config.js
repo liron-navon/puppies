19161d1c33f8c5738a083bd92e014d6813785e8e
liron-navon <liron.navon@rabobank.nl>
2021-10-01 15:22:51 +0200

    pupeteer...

--- packages/components/jest-puppeteer.config.js
+++ packages/components/jest-puppeteer.config.js
@@ -1,8 +1,23 @@
-module.exports = {
+// module.exports = {
+//     launch: {
+//       headless: true,
+//       ignoreDefaultArgs: ["--disable-extensions"],
+//       args: ["--no-sandbox"],
+//     //   executablePath: "chrome.exe"
+//     }
+//   };
+
+  module.exports = {
     launch: {
-      headless: true,
-      ignoreDefaultArgs: ["--disable-extensions"],
-      args: ["--no-sandbox"],
-    //   executablePath: "chrome.exe"
-    }
-  };
+      dumpio: true,
+      headless: true
+    },
+    browser: 'chromium',
+    // browserContext: 'default',
+    // server: {
+    //   command: `npm start`,
+    //   port: 3000,
+    //   launchTimeout: 10000,
+    //   debug: true,
+    // },
+  }
