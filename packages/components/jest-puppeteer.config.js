d001eb1bb47bc08fb26c22e7ebe49623d98ea1b8
liron-navon <liron.navon@rabobank.nl>
2021-10-02 12:23:38 +0200

    ci

--- packages/components/jest-puppeteer.config.js
+++ packages/components/jest-puppeteer.config.js
@@ -3,13 +3,13 @@ const port = process.env.PORT || 6006;
 let options = {
     launch: {
         headless: true,
-        args: [
+        args: process.env.CI ? [
           '--ignore-certificate-errors',
           '--no-sandbox',
           '--disable-setuid-sandbox',
           '--disable-accelerated-2d-canvas',
           '--disable-gpu'
-              ]
+              ] : []
     },
     server: {
         command: `start-storybook -p ${port} --ci --quiet`,
