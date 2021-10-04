c7594e85a19ab7e1dbeacb08b9531b66ad52cb4d
Liron Navon <lironavon42@gmail.com>
2021-10-04 13:21:30 +0300

    improved ci and documentation (#22)
    * improved ci and documentation
    
    * removed unneeded title component
    
    Co-authored-by: liron-navon <liron.navon@rabobank.nl>

--- packages/app/src/web-components.ts
+++ packages/app/src/web-components.ts
@@ -1,11 +1,10 @@
-import { TitleComponentAttributes, ImageUploadComponentAttributes, LazyLoadedGalleryComponentAttributes } from 'puppies-components';
+import { ImageUploadComponentAttributes, LazyLoadedGalleryComponentAttributes } from 'puppies-components';
 import 'puppies-components';
 
 // provides type definitions for the web components
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'pup-title': TitleComponentAttributes;
       'pup-lazy-loaded-gallery': LazyLoadedGalleryComponentAttributes
       'pup-image-upload': ImageUploadComponentAttributes
     }
--- packages/components/src/components.ts
+++ packages/components/src/components.ts
@@ -1,3 +1,2 @@
-export * from './components/title/title.component';
 export * from './components/lazy-loaded-gallery/lazy-loaded-gallery.component';
 export * from './components/image-upload/image-upload.component';
